b24de25aa38ac856dae8efe3dfc5194e0811fe6b
waashii <70151834+waashii@users.noreply.github.com>
2022-03-19 10:37:52 +0300

    I think I'm done🙃

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,11 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+struct Student {
+var name: String
+var grade:Int
+var attendence: Bool
+}
+ var Aisha = Student(name: "Aisha", grade: 90, attendence: true)
+
+var Washi = Student(name: "Washi", grade: 95, attendence: true)
